611e77cc82c5ef475359fc0ed32b2ad932729e69
Vult <Vult@sad.com>
2022-09-07 18:01:04 +0300

    provider

---
+++ lesson2/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.2.8"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "lessonvult"
+  region  = "us-east-1"
+}
